627e550f99fcda1adb21cd8ba3ff8108a7076908
AnkitGourav <Ankitgourav035@gmail.com>
2019-02-15 01:21:01 +0530

    init ion overlays
    Special Thanks to https://github.com/Mazda-- and https://github.com/jhenrique09 and other devs who made all these beautiful themes.

---
+++ accents/maniaamber/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_RRO_THEME := ManiaAmber
+LOCAL_PACKAGE_NAME := ManiaAmber
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_RRO_PACKAGE)
---
+++ config.mk
@@ -0,0 +1,93 @@
+# accents
+PRODUCT_PACKAGES += \
+    Amber \
+    Black \
+    Blue \
+    BlueGrey \
+    Brown \
+    CandyRed \
+    Cyan \
+    DeepOrange \
+    DeepPurple \
+    ExtendedGreen \
+    Green \
+    Grey \
+    Indigo \
+    JadeGreen \
+    LightBlue \
+    LightGreen \
+    Lime \
+    Orange \
+    PaleBlue \
+    PaleRed \
+    Pink \
+    Purple \
+    Red \
+    Teal \
+    Yellow \
+    White \
+    UserOne \
+    UserTwo \
+    UserThree \
+    UserFour \
+    UserFive \
+    UserSix \
+    UserSeven \
+    ObfusBleu \
+    NotImpPurple \
+    Holillusion \
+    MoveMint \
+    FootprintPurple \
+    BubblegumPink \
+    FrenchBleu \
+    Stock \
+    ManiaAmber \
+    SeasideMint \
+    DreamyPurple \
+    SpookedPurple \
+    HeirloomBleu \
+    TruFilPink \
+    WarmthOrange \
+    ColdBleu \
+    DiffDayGreen \
+    DuskPurple \
+    BurningRed \
+    HazedPink \
+    ColdYellow \
+    NewHouseOrange \
+    IllusionsPurple
+
+# dark
+PRODUCT_PACKAGES += \
+    SettingsDark \
+    SystemDark \
+    SystemUIDark \
+    GBoardDark \
+    WellbeingDark
+
+# light
+PRODUCT_PACKAGES += \
+    GBoardLight
+
+# qs
+PRODUCT_PACKAGES += \
+    QStileDefault \
+    QStileCircleTrim \
+    QStileCircleDualTone \
+    QStileCircleGradient \
+    QStileCookie \
+    QStileDottedCircle \
+    QStileDualToneCircle \
+    QStileInk \
+    QStileInkdrop \
+    QStileMountain \
+    QStileNinja \
+    QStileOreo \
+    QStileOreoCircleTrim \
+    QStileOreoSquircleTrim \
+    QStilePokesign \
+    QStileSquaremedo \
+    QStileSquircle \
+    QStileSquircleTrim \
+    QStileTeardrop \
+    QStileWavey
