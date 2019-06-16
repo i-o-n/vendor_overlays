0e774ec1004a5321e4e0000ddd2be3102424825b
Stallix <thestallix@gmail.com>
2019-06-17 01:55:59 +0530

    QS Tiles: Add Android Q Beta 4 Tile Styles

---
+++ qs/qstilesquare/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_RRO_THEME := QStileSquare
+LOCAL_PACKAGE_NAME := QStileSquare
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_RRO_PACKAGE)
